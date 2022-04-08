ad1d2a97bfc95ee4b9a1a13989596e249fc0fd5c
Tomáš Kryčfalušij <91247418+TomasKrycfalusij@users.noreply.github.com>
2022-04-08 17:50:30 +0200

    Počáteční soubory projektu MakeCode

---
+++ main.py
@@ -0,0 +1,18 @@
+x = 0
+y = 0
+displej = 5
+
+for i in range(displej):
+    led.plot(x, y)
+    x += 1
+
+def on_button_pressed_a():
+    global x, y
+    while True:
+        basic.clear_screen()
+        y += 1
+        for i in range(displej):
+            x += 1
+            led.plot(x%5, y%5)
+        basic.pause(1000)
+input.on_button_pressed(Button.A, on_button_pressed_a)
---
+++ main.ts
@@ -0,0 +1,20 @@
+let i: number;
+let x = 0
+let y = 0
+let displej = 5
+for (i = 0; i < displej; i++) {
+    led.plot(x, y)
+    x += 1
+}
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    
+    while (true) {
+        basic.clearScreen()
+        y += 1
+        for (let i = 0; i < displej; i++) {
+            x += 1
+            led.plot(x % 5, y % 5)
+        }
+        basic.pause(1000)
+    }
+})
